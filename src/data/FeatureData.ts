1ff0b597caa5422155674da106edf2d36bfa02b1
Xristos <xrsapostolidis@gmail.com>
2025-09-08 12:11:50 +0300

    Add FeatureData

---
+++ src/data/FeatureData.ts
@@ -0,0 +1,32 @@
+export type featureTab = ["Privacy", "Speed", "Security", "Service"];
+
+export const featuresData = [
+  {
+    id: 1,
+    title: "Privacy",
+    description:
+      "GhostVPN hides your IP and encrypts your data, keeping you invisible online. No trackers, no footprints—just true digital freedom.",
+    image: "./MainFeatures/privacy.jpg",
+  },
+  {
+    id: 2,
+    title: "Speed",
+    description:
+      "Enjoy lightning-fast speeds with GhostVPN. Stream, download, and browse without interruptions.",
+    image: "./MainFeatures/speed.jpg",
+  },
+  {
+    id: 3,
+    title: "Security",
+    description:
+      "Your security is our priority. GhostVPN uses advanced encryption protocols to keep your data safe.",
+    image: "./MainFeatures/security.jpg",
+  },
+  {
+    id: 4,
+    title: "Service",
+    description:
+      "Experience top-notch customer service with GhostVPN. We're here to help you 24/7.",
+    image: "./MainFeatures/support.jpg",
+  },
+];
